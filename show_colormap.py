b2b330d2c513578fa5d3ddc1f46c2d1a58e79a0d
Leonid Benkevitch <lbenkev@gmail.com>
2020-06-17 20:02:01 -0400

    ...

--- show_colormap.py
+++ show_colormap.py
@@ -5,6 +5,8 @@ show_colormap.py
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+from matplotlib.patches import Rectangle
+from matplotlib.collections import PatchCollection
 
 #
 # For indication of goodness of the multiple correlation coefficient,
@@ -19,24 +21,55 @@ rYlGn = ListedColormap(cmYlGn(np.linspace(0.0, 0.6, 256)), name='rYlGn')
 #
 cmhot_r = plt.cm.get_cmap('hot_r')
 hotr = ListedColormap(cmhot_r(np.linspace(0.0, 0.7, 256)), name='hotr')
+cmap = ListedColormap([
+    '#08306b', '#08519c', '#2171b5', '#4292c6',
+    '#6baed6', '#9ecae1', '#c6dbef', '#deebf7',
+    '#fee0d2', '#fcbba1', '#fc9272', '#fb6a4a',
+    '#ef3b2c', '#cb181d', '#a50f15', '#67000d',
+])
 
-x = np.arange(256)
-y = np.zeros(256)
-cols = plt.cm.jet(x)
+cmap1 = ListedColormap([
+    '#08306b', '#08519c', '#2171b5', '#4292c6',
+    '#6baed6', '#9ecae1', '#c6dbef', '#deebf7',
+    '#fee0d2', '#fcbba1', '#fc9272', '#fb6a4a',
+    '#ef3b2c', '#cb181d', '#a50f15'
+])
+
+cmjet = plt.cm.get_cmap('jet')
+lcmjet = ListedColormap(cmjet(np.linspace(0.1, 0.9, 16)), name='lcmjet')
+
+cmhsv = plt.cm.get_cmap('hsv')
+lcmhsv = ListedColormap(cmhsv(np.linspace(0., 0.35, 8)), name='lcmhsv')
+
+
+r = np.linspace(1, 0, 16)
+g = np.linspace(0, 1, 16)
+gr = np.zeros((16,4)) 
+gr[:,0] = r
+gr[:,1] = g
+gr[:,2] = 0 
+gr[:,3] = 1
+
+lcmgr = ListedColormap(gr)
+
+
 iplot = 1
 
-fig = plt.figure(figsize=(14,8))
+# fig = plt.figure(figsize=(14,8))
+fig = plt.figure()
 
-for colmap in [rYlGn, hotr]:
-    ax = plt.subplot(2, 1, iplot)
 
-    for icol in range(256):
-        ax.plot(x, y, marker='s', markersize=20, \
-                # markerfacecolor=rYlGn.colors[icol]) 
-                markerfacecolor=cols[icol]) 
-    ax.set_xlim(0,255)
-    ax.set_ylim(-1,+1)
+for colmap in [rYlGn, hotr, lcmhsv, lcmgr]:
+    ax = fig.add_subplot(4, 1, iplot, xlim=(0,255), ylim=(0,1))
+    #ax.set_axis_off()
+    col = PatchCollection([Rectangle((x, 0), 1, 1) for x in range(256)])
+    col.set_array(np.arange(256))
+    col.set_cmap(colmap)
+    ax.add_collection(col)
+
     iplot += 1
 
 
 fig.show()    
+
+
